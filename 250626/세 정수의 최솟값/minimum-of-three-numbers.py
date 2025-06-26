478e76d71af4961087b3b7c3a35f3b30d9e7b3d0
SuJeongHong <56856010+SuJeongHong@users.noreply.github.com>
2025-06-26 22:51:16 +0900

    Title: 세 정수의 최솟값, Time: 51ms, Memory: 16MB, Status: Passed - Codetree

--- 250626/세 정수의 최솟값/minimum-of-three-numbers.py
+++ 250626/세 정수의 최솟값/minimum-of-three-numbers.py
@@ -2,9 +2,9 @@ a,b,c=map(int, input().split())
 small=0
 if a<=b and a<=c:
     small=a
-if b<=a and b<=c:
+elif b<=a and b<=c:
     small=b
-if c<=a and c<=b:
+elif c<=a and c<=b:
     small=c
     
 print(small)
