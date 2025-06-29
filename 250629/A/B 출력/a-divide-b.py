5068d9c6f1a9111b280013f04f54de9e277c6526
SuJeongHong <56856010+SuJeongHong@users.noreply.github.com>
2025-06-29 23:30:55 +0900

    Title: A/B 출력, Time: 44ms, Memory: 16MB, Status: Passed - Codetree

---
+++ 250629/A/B 출력/a-divide-b.py
@@ -0,0 +1,11 @@
+a,b=map(int, input().split())
+if a // b == 0:
+    c = '0.'
+else:
+    c = str(a // b) + '.'
+a = a % b
+for _ in range(20):
+    a = a * 10
+    c += str(a // b)
+    a = a % b 
+print(c)
